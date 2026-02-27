beed83ae6ccd04cbeb59197c042513788852532e
user3409328493284 <jamie.stauffer@sbw-media.ch>
2026-02-27 11:04:43 +0100

    Update title in index.php to correct project name

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>m307</title>
+    <title>Modul 307</title>
 </head>
 <body>
     
